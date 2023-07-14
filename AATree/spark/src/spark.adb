7a105c360d2dcbc5ffaa518aec124b0fdb56b882
Chris Eberly <ceberly@users.noreply.github.com>
2023-07-14 14:28:31 -0400

    save work

--- AATree/spark/src/spark.adb
+++ AATree/spark/src/spark.adb
@@ -5,6 +5,7 @@ procedure Spark with
    SPARK_Mode => On
 is
    type AATree;
+
    type Tree_Ptr is access AATree;
 
    type AATree is limited record
@@ -14,67 +15,97 @@ is
       Level : Positive;
    end record;
 
-   procedure Skew (Tree : in out Tree_Ptr) with
-      Global => null,
-      Pre    => Tree /= null,
-      Post   => Tree /= null
+   type Tree_Container is limited record
+      Root : Tree_Ptr;
+   end record;
+
+   function Has_No_Left_Red_Child (Tree : access AATree) return Boolean is
+     (Tree.Left = null or else Tree.Left.Level < Tree.Level) with
+      Ghost,
+      Pre => Tree /= null;
+
+--   function Skew (Tree : Tree_Ptr) return Tree_Ptr with
+--      Global => null,
+--      Pre    => Tree /= null
+--   is
+--      L : Tree_Ptr;
+--   begin
+--      if Tree.Left /= null and then Tree.Left.Level = Tree.Level then
+--         L         := Tree.Left;
+--         Tree.Left := L.Right;
+--         L.Right   := Tree;
+--
+--         return L;
+--      end if;
+--
+--      return Tree;
+--   end Skew;
+--
+--   function Split (Tree : Tree_Ptr) return Tree_Ptr with
+--      Global => null,
+--      Pre    => Tree /= null
+--   is
+--      L : Tree_Ptr;
+--   begin
+--      if Tree.Right /= null and then Tree.Right.Right /= null
+--        and then Tree.Right.Right.Level = Tree.Level
+--      then
+--         L          := Tree.Right;
+--         Tree.Right := L.Left;
+--         L.Left     := Tree;
+--         --   Assume we have a sane sized tree for now to prevent overflow ...
+--         pragma Assume (L.Level < Natural'Last / 2);
+--         L.Level := L.Level + 1;
+--
+--         return L;
+--      end if;
+--
+--      return Tree;
+--   end Split;
+--
+   --   https://docs.adacore.com/spark2014-docs/html/ug/en/source/access.html#borrowing
+   procedure Insert_Existing (Tree : access AATree; K : Positive) with
+      Pre => Tree /= null
    is
-      L : Tree_Ptr;
+      X : access AATree := Tree;
    begin
-      if Tree.Left = null then
-         return;
-      end if;
+      while X /= null loop
+         if K < X.Key then
+            if X.Left = null then
+               X.Left := new AATree'(K, null, null, 1);
+               -- X now refers to the parent of the new node
+               -- which is where we want to start Split (Skew (X))...
+               exit;
+            end if;
 
-      if Tree.Left.Level = Tree.Level then
-         L         := Tree.Left;
-         Tree.Left := L.Right;
-         L.Right   := Tree;
-         Tree      := L;
-      end if;
-   end Skew;
+            X := X.Left;
+         else
+            if X.Right = null then
+               X.Right := new AATree'(K, null, null, 1);
+               -- " "
+               exit;
+            end if;
 
-   procedure Split (Tree : in out Tree_Ptr) with
-      Global => null,
-      Pre    => Tree /= null,
-      Post   => Tree /= null
-   is
-      L : Tree_Ptr;
-   begin
-      if Tree.Right = null or else Tree.Right.Right = null then
-         return;
-      end if;
+            X := X.Right;
+         end if;
+      end loop;
 
-      if Tree.Right.Right.Level = Tree.Level then
-         L          := Tree.Right;
-         Tree.Right := L.Left;
-         L.Left     := Tree;
-         --   Assume we have a sane sized tree for now to prevent overflow ...
-         pragma Assume (L.Level < Natural'Last / 2);
-         L.Level := L.Level + 1;
+--      while X /= Original_Root loop
+--         X := Split (Skew (X));
+--      end loop;
+--
+--      X := Split (Skew (X));
+   end Insert_Existing;
 
-         Tree := L;
-      end if;
-   end Split;
-
-   procedure Insert (Tree : in out Tree_Ptr; K : Positive) with
-      Global => null,
-      Post   => Tree /= null
+   procedure Insert (Tree : in out Tree_Container; K : Positive) with
+      Post => Tree.Root /= null
    is
    begin
-      if Tree = null then
-         Tree :=
-           new AATree'(Key => K, Left => null, Right => null, Level => 1);
-         return;
-      end if;
-
-      if K < Tree.Key then
-         Insert (Tree.Left, K);
+      if Tree.Root = null then
+         Tree.Root := new AATree'(K, null, null, 1);
       else
-         Insert (Tree.Right, K);
+         Insert_Existing (Tree.Root, K);
       end if;
-
-      Skew (Tree);
-      Split (Tree);
    end Insert;
 
    --  Print from a dereferenced AATree to avoid confusing SPARK
@@ -86,25 +117,25 @@ is
          return;
       end if;
 
-      if Tree.Left /= null then
-         Print (Tree.Left.all, Space & "  ");
+      if Tree.Right /= null then
+         Print (Tree.Right.all, Space & "  ");
       end if;
 
       Put_Line (To_String (Space & Tree.Key'Image));
 
-      if Tree.Right /= null then
-         Print (Tree.Right.all, Space & "  ");
+      if Tree.Left /= null then
+         Print (Tree.Left.all, Space & "  ");
       end if;
    end Print;
 
-   Root : Tree_Ptr := null;
+   Tree : Tree_Container;
 
    Input : constant array (Positive range <>) of Integer :=
      (81, 99, 10, 32, 8, 19, 3, 78);
 begin
    for Number in Input'Range loop
-      Insert (Root, Input (Number));
+      Insert (Tree, Input (Number));
    end loop;
 
-   Print (Root.all, To_Unbounded_String (""));
+   Print (Tree.Root.all, To_Unbounded_String (""));
 end Spark;
